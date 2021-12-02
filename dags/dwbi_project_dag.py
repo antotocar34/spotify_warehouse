3960f1cc9a4a14a0571b31a4f77e6355e878f044
Antoine Carnec <antoinecarnec@gmail.com>
2021-12-02 14:46:23 +0100

    Added extract_db task

--- dags/dwbi_project_dag.py
+++ dags/dwbi_project_dag.py
@@ -1,56 +1,74 @@
-import sqlalchemy
-import pandas as pd 
-from sqlalchemy.orm import sessionmaker
+import os
 import requests
 import json
-import sqlite3
-from datetime import datetime,timedelta
-from airflow.decorators import dag, task 
-import os
+from typing import List
+from datetime import datetime, timedelta
+from uuid import uuid4
+from pickle import dump
+from pathlib import Path
 
+from airflow.decorators import dag, task
+import sqlite3
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+import pandas as pd
 
 default_args = {
-    'owner'         : 'dj',
-    'retries'       : 5,
-    'retry_delay'   : timedelta(minutes=2)
+    "owner": "dj",
+    "retries": 5,
+    "retry_delay": timedelta(minutes=2),
 }
 
-@dag(dag_id = 'DWBI_finalproject_dag',
-    default_args = default_args,
-    description = 'This is our DWBI final project ETL DAG.',
-    start_date= datetime(2021, 12, 10, 2), 
-    schedule_interval='@daily')
 
-def final_etl():
-
+@dag(
+    dag_id="DWBI_finalproject_dag",
+    default_args=default_args,
+    description="This is our DWBI final project ETL DAG.",
+    start_date=datetime(2021, 12, 1, 2),
+    schedule_interval="@daily",
+)
+def etl():
     @task()
-    def extract():
+    def extract_db() -> List[str]:
         # Establishing a connection to the database
-        cnx = sqlite3.connect('playlist.db')
+        cnx = sqlite3.connect("playlist.db")
         print("Successfully made contact with the database.")
-        
+
         # Defining the query of interest
-        select = 'SELECT * FROM car'
+        tables = ["songs", "playlists", "playlistssongs"]
+        selects = [
+            f"SELECT * FROM {table}"
+            for table in tables
+        ]
 
         # Executing the query
-        query = cnx.execute(select)
+        queries = [cnx.execute(select) for select in selects]
+
 
         # Taking the output from the query and putting into Pandas dataframe for processing
-        cols = [column[0] for column in query.description]
-        results= pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
-        print('Successfully placed data in dataframe.')
+        def df_from_query(query):
+            cols = [column[0] for column in query.description]
+            result_df = pd.DataFrame.from_records(
+                data=query.fetchall(), columns=cols
+            )
+            print("Successfully placed data in dataframe.")
+            return result_df
+
+        dfs = [df_from_query(query) for query in queries]
 
         # Closing our database connection
         cnx.close()
 
         # Saving results to a csv file that can then be read by another function
-        identifier = str(datetime.date(datetime.now()))
-        cwd = os.getcwd()
-        path = cwd + "/" + identifier
-        results.to_csv(path)
-        print('Saved file for future use to {}'.format(path))
-        return identifier
+        OUT_PATH = Path(f"{os.getenv('AIRFLOW_HOME')}/data/") 
+        assert OUT_PATH.exists(), "Data output path is not found"
+        for df, table_name in zip(dfs, tables):
+            with open(OUT_PATH / f"{table_name}.pkl", 'wb') as f:
+                dump(df, f)
 
-    id = extract()
-    
-etl = final_etl()
+        print(f"Saved file for future use to {str(OUT_PATH)}")
+        return tables
+
+    table_ids = extract_db()
+
+etl_out = etl()
