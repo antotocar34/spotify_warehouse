f35c52d069c1721a8b86956cee8b8920eacc01b1
Antoine Carnec <antoinecarnec@gmail.com>
2021-12-07 14:37:02 +0100

    ETL runs!

--- dags/dwbi_project_dag.py
+++ dags/dwbi_project_dag.py
@@ -1,5 +1,4 @@
 import os
-
 import requests
 import json
 from typing import List
@@ -25,6 +24,7 @@ AIRFLOW_HOME = os.getenv("AIRFLOW_HOME")
 assert AIRFLOW_HOME, "AIRFLOW_HOME environemnt variable is not set."
 DATA_HOME = Path(f"{AIRFLOW_HOME}/data")
 assert DATA_HOME.exists()
+FACT_DIMENSION_DB_PATH = f"{DATA_HOME}/factdb/data_warehouse.db"
 
 
 @dag(
@@ -37,6 +37,9 @@ assert DATA_HOME.exists()
 def etl():
     @task()
     def extract_db() -> List[str]:
+        """
+        This task extracts from the database tables
+        """
         # Establishing a connection to the database
         cnx = sqlite3.connect(f"{AIRFLOW_HOME}/playlist.db")
         print("Successfully made contact with the database.")
@@ -44,13 +47,17 @@ def etl():
         # Defining the query of interest
         tables = ["songs", "playlists", "playlistssongs"]
         # Select everythin from the database
-        selects = [f"SELECT * FROM {table}" for table in tables]
+        selects = {table: f"SELECT * FROM {table}" for table in tables}
 
         # Executing the query
-        queries = [cnx.execute(select) for select in selects]
+        queries = {
+            name: cnx.execute(select) for name, select in selects.items()
+        }
 
-        # Taking the output from the query and putting into Pandas dataframe for processing
         def df_from_query(query):
+            """
+            Takes a query and stores it into a pandas dataframe.
+            """
             cols = [column[0] for column in query.description]
             result_df = pd.DataFrame.from_records(
                 data=query.fetchall(), columns=cols
@@ -58,7 +65,7 @@ def etl():
             print("Successfully placed data in dataframe.")
             return result_df
 
-        dfs = {name: df_from_query(query) for query, name in zip(queries, tables)}
+        dfs = {name: df_from_query(query) for name, query in queries.items()}
 
         # Closing our database connection
         cnx.close()
@@ -75,23 +82,30 @@ def etl():
 
     @task()
     def extract_api_data() -> List[str]:
-        """ """
-        # TODO Parse json
+        """
+        Extract api data from csv.
+        Store as dataframe and
+        return a list of Paths.
+        """
         assert os.getenv("AIRFLOW_HOME")
         api_datas = [
             "audio_features.csv",
             "audio_features_top.csv",
             "toptracks.csv",
         ]
-        paths = [(DATA_HOME / Path(s)).exists() for s in api_datas]
-        assert all(paths), print(paths)
-        return api_datas
+        paths = [(DATA_HOME / Path(s)) for s in api_datas]
+        assert all(map(lambda p: p.exists(), paths)), print(paths)
+        for p in paths:
+            df = pd.read_csv(p)
+            df.to_pickle(p.parent.resolve() / f"api_{p.name}")
+        return [str(p.parent / f"api_{p.name}") for p in paths]
 
     @task()
     def transform_data(table_pickles: List[str], api_data_csvs: List[str]):
         """
         Create fact and dimension tables.
         """
+        # Define audio feature names
         audio_features = [
             "danceability",
             "energy",
@@ -111,90 +125,196 @@ def etl():
             pd.read_pickle(f"{DATA_HOME}/interrim/{table}")
             for table in table_pickles
         ]
-        api_data_dfs = [
-            pd.read_csv(f"{DATA_HOME}/{csv}") for csv in api_data_csvs
-        ]
+        api_data_dfs = [pd.read_pickle(path) for path in api_data_csvs]
 
+        # Unpack the dataframes
         songs_table_df = table_dfs[0]
-        audio_features_df = api_data_dfs[0]
         playlists_table_df = table_dfs[1]
         pssongs_df = table_dfs[2]
-
-        playlist_fact = pssongs_df.merge(audio_features_df, left_on="track_uri", right_on="song_uri", how="left") \
-                                      .drop(["song_uri", "Unnamed: 0", "track_href", "analysis_url", "id"], axis=1) \
-                                      .merge(playlists_table_df, how="left", on="playlist_id") \
-                                      .merge(songs_table_df[["artist_uri", "track_uri"]], on="track_uri", how="left") \
-                                      .drop("playlist_name", axis=1)
-
-        # playlist_fact["song_popularity_per_playlist"] = playlist_fact["track_uri"].map(dict(pssongs_df.track_uri.value_counts()))
-
-
-        dimension_songs = audio_features_df[["song_uri", "duration_ms"]].merge(
-                songs_table_df[["track_uri", "track_name"]], how="right", left_on="song_uri", right_on="track_uri"
-                ).drop("song_uri", axis=1)
-
-
-        dimension_playlists = playlists_table_df.drop("num_followers", axis=1)
-
-        dimension_artists = songs_table_df[["artist_uri", "artist_name"]].drop_duplicates()
-
+        audio_features_df = api_data_dfs[0]
         audio_top_tracks_df = api_data_dfs[1]
         top_tracks_df = api_data_dfs[2].drop_duplicates(subset="track_uri")
 
-        top_track_fact = top_tracks_df[["track_uri", "artist", "popularity"]].merge(
-                audio_top_tracks_df[audio_features + ["track_uri"]], how="left", on="track_uri"
-                ).rename(
-                        columns={"artist": "artist_uri",
-                                 "spotify": "spotify_popularity"}
-                        )
-                                                                                
+        # Make the playlist fact table
+        playlist_fact = (
+            pssongs_df.merge(
+                audio_features_df,
+                left_on="track_uri",
+                right_on="song_uri",
+                how="left",
+            )
+            .drop(
+                ["song_uri", "Unnamed: 0", "track_href", "analysis_url", "id"],
+                axis=1,
+            )
+            .merge(playlists_table_df, how="left", on="playlist_id")
+            .merge(
+                songs_table_df[["artist_uri", "track_uri"]],
+                on="track_uri",
+                how="left",
+            )
+            .drop("playlist_name", axis=1)
+        )
 
-        dimension_top_tracks = top_tracks_df[["track_uri", "track_name", "explicit", "duration_ms"]]
-        dimension_top_artists = top_tracks_df[["artist", "artist_name"]].rename({"artist":"artist_uri"}, axis=1) \
-                                                                        .drop_duplicates(subset="artist_uri")
+        # Make the dimension tables
+        dimension_songs = (
+            audio_features_df[["song_uri", "duration_ms"]]
+            .merge(
+                songs_table_df[["track_uri", "track_name"]],
+                how="right",
+                left_on="song_uri",
+                right_on="track_uri",
+            )
+            .drop("song_uri", axis=1)
+        )
 
+        dimension_playlists = playlists_table_df.drop("num_followers", axis=1)
+
+        dimension_artists = songs_table_df[
+            ["artist_uri", "artist_name"]
+        ].drop_duplicates()
+
+        # Making top track fact table
+        top_track_fact = (
+            top_tracks_df[["track_uri", "artist", "popularity"]]
+            .merge(
+                audio_top_tracks_df[audio_features + ["track_uri"]],
+                how="left",
+                on="track_uri",
+            )
+            .rename(
+                columns={
+                    "artist": "artist_uri",
+                    "spotify": "spotify_popularity",
+                }
+            )
+        )
+
+        dimension_top_songs = top_tracks_df[
+            ["track_uri", "track_name", "explicit", "duration_ms"]
+        ]
+        dimension_top_artists = (
+            top_tracks_df[["artist", "artist_name"]]
+            .rename({"artist": "artist_uri"}, axis=1)
+            .drop_duplicates(subset="artist_uri")
+        )
 
         facts: List[pd.DataFrame] = [playlist_fact, top_track_fact]
 
-        dimensions: List[pd.DataFrame] = [dimension_playlists,
-                                          dimension_artists,
-                                          dimension_songs,
-                                          dimension_top_tracks,
-                                          dimension_top_artists]
+        dimensions: List[pd.DataFrame] = [
+            dimension_playlists,
+            dimension_artists,
+            dimension_songs,
+            dimension_top_songs,
+            dimension_top_artists,
+        ]
 
-        
-        pkl_fact_names = list(map(lambda s: f"fact_{s}", ["playlist", "top_track"]))
-        pkl_dimension_names = list(map(lambda s: f"dimension_{s}", ["playlists", "artists", "songs", "top_songs", "top_artists"]))
-        for d, name in zip(dimensions + facts, pkl_dimension_names + pkl_fact_names):
+        pkl_fact_names = list(
+            map(lambda s: f"fact_{s}", ["playlist", "top_track"])
+        )
+        pkl_dimension_names = list(
+            map(
+                lambda s: f"dimension_{s}",
+                ["playlists", "artists", "songs", "top_songs", "top_artists"],
+            )
+        )
+        for d, name in zip(
+            dimensions + facts, pkl_dimension_names + pkl_fact_names
+        ):
             d.to_csv(f"{DATA_HOME}/interrim/{name}.csv")
 
         return pkl_dimension_names + pkl_fact_names
 
     @task
     def load(csv_names: List[str]):
+        """
+        Load data into a databse that is based on the star schema.
+        """
 
-        dfs = [pd.read_csv(f"{DATA_HOME}/interrim/") for csv_name in csv_names]
+        dfs = [
+            pd.read_csv(f"{DATA_HOME}/interrim/{csv_name}.csv", index_col=0)
+            for csv_name in csv_names
+        ]
 
-        dimension_playlists, dimension_songs, dimension_top_tracks, dimension_top_artists = dfs[0:3]
+        (
+            dimension_playlists,
+            dimension_artists,
+            dimension_songs,
+            dimension_top_songs,
+            dimension_top_artists,
+        ) = dfs[0:5]
+        fact_playlist, fact_top_track = dfs[5:7]
+
+        table_name_mapping = {
+            "dimension_playlists": dimension_playlists,
+            "dimension_artists": dimension_artists,
+            "dimension_songs": dimension_songs,
+            "dimension_top_songs": dimension_top_songs,
+            "dimension_top_artists": dimension_top_artists,
+            "fact_playlist": fact_playlist,
+            "fact_top_track": fact_top_track,
+        }
 
         def create_connection(db_file):
-            """ create a database connection to a SQLite database """
+            """create a database connection to a SQLite database"""
             conn = None
             try:
                 conn = sqlite3.connect(db_file)
+                with open(f"{AIRFLOW_HOME}/db2_creation.txt", "r") as f:
+                    create_statement = f.read()
+                conn.executescript(create_statement)
                 print(sqlite3.version)
             except Error as e:
-                print(e)
+                print(f"ERROR: {e}")
             return conn
-        conn = create_connection(f"{AIRFLOW_HOME}/data/factdb/factdb.db")
 
+        data_base_path = f"{AIRFLOW_HOME}/data/factdb/data_warehouse.db"
+        conn = create_connection(data_base_path)
+        cursor = conn.cursor()
 
+        def insert_df_into_database(cursor, conn, table: str):
+            out = cursor.execute(f"SELECT * FROM {table} limit1")
+            # Get attributes/columns of a table.
+            column_names = [row[0] for row in out.description]
+            df = table_name_mapping[table]
+            # assert that columns of dataframe are the same as the attributes of the table
+            assert list(df.columns) == column_names, set(
+                df.columns
+            ).symmetric_difference(set(column_names))
 
+            tuples = [tuple(row) for _, row in df.iterrows()]
 
+            sql = f"INSERT OR IGNORE INTO {table} values ({', '.join(['?' for _ in column_names])})"
+            print(f"Inserted into {table}")
+            print(sql, tuples[0])
+            cursor.executemany(sql, tuples)
+            conn.commit()
+            return
+
+        for table in table_name_mapping:
+            insert_df_into_database(cursor, conn, table)
+
+        conn.close()
+
+        return list(table_name_mapping.keys())
+    
+    @task()
+    def shift_to_Qlik(table_names: List[str]):
+
+        conn = sqlite3.connect(FACT_DIMENSION_DB_PATH)
+
+        for table in table_names:
+            table_df = pd.read_sql_query(f"SELECT * FROM {table}", conn)
+            table_df.to_csv(f"./data/qlik/{table}.csv")
+
+        return
 
     table_pickles = extract_db()
     api_data_csvs = extract_api_data()
-    out = transform_data(table_pickles, api_data_csvs)
+    csv_names = transform_data(table_pickles, api_data_csvs)
+    table_names = load(csv_names)
+    out = shift_to_Qlik(table_names)
+    
 
 
 etl_out = etl()
